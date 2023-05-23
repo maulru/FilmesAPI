f0fe49a3d486c0b3a5f0881316a5506b162f6f90
Mauro <maurojr_@outlook.com>
2023-05-22 22:38:20 -0300

    Implementação do método para retornar um ou mais filme

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="O titulo do filme é obrigatório")]
+    public String Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatória")]
+    [Range (50, 600, ErrorMessage = "A duração deve ser entre 50 e 600 minutos")]
+    public int Duracao { get; set; }
+}
