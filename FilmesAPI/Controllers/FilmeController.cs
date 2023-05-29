d3cf67c112ddfd93413ece93102e502c8b35acbb
Mauro <maurojr_@outlook.com>
2023-05-28 22:40:12 -0300

    Implementação do método DELETE

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -33,11 +33,11 @@ namespace FilmesAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0,
+        public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0,
             [FromQuery] int take = 50)
         {
-
-            return _context.Filmes.Skip(skip).Take(take);
+            return _mapper.Map<List<ReadFilmeDto>>
+                (_context.Filmes.Skip(skip).Take(take));
         }
 
         [HttpGet("{id}")]
@@ -46,6 +46,7 @@ namespace FilmesAPI.Controllers
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
             if (filme == null) return NotFound();
+            var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
             return Ok(filme);
         }
 
@@ -81,6 +82,16 @@ namespace FilmesAPI.Controllers
             _context.SaveChanges();
             return NoContent();
 
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) return NotFound();
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
         }
 
     }
---
+++ FilmesAPI/Data/DTOs/ReadFilmeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmesAPI.Data.DTOs
+{
+    public class ReadFilmeDto
+    {
+        public String Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+    }
+}
