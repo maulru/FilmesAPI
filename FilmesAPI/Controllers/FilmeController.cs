60bbe6549918f0f1ba70a1166c0626d309f35bf3
Mauro <maurojr_@outlook.com>
2023-05-28 23:29:19 -0300

    Descrição dos métodos inclusa no swagger

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -21,7 +21,14 @@ namespace FilmesAPI.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Adiciona um filme
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
             Filme filme = _mapper.Map<Filme>(filmeDto);
@@ -32,7 +39,13 @@ namespace FilmesAPI.Controllers
                 filme);
         }
 
+        /// <summary>
+        /// Retorna filmes cadastrados 
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso o retorno seja feita com sucesso</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0,
             [FromQuery] int take = 50)
         {
@@ -40,7 +53,14 @@ namespace FilmesAPI.Controllers
                 (_context.Filmes.Skip(skip).Take(take));
         }
 
+        /// <summary>
+        /// Retorna filme por ID
+        /// </summary>
+        /// <param name="id">Parâmetro necessário para retornar um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso o retorno seja feita com sucesso</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult? RecuperaFilmePorId(int id)
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
@@ -50,7 +70,14 @@ namespace FilmesAPI.Controllers
             return Ok(filme);
         }
 
+        /// <summary>
+        /// Altera um filme
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para alteração de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso alteração seja feita com sucesso</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto){
 
             var filme=_context.Filmes.FirstOrDefault(filme => filme.Id == id);
@@ -61,7 +88,13 @@ namespace FilmesAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Altera um filme
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso alteração seja feita com sucesso</response>
         [HttpPatch("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult AtualizaFilmeParcial(int id, 
             JsonPatchDocument<UpdateFilmeDto> patch)
         {
@@ -84,7 +117,14 @@ namespace FilmesAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Deleta um filme
+        /// </summary>
+        /// <param name="id">Objeto com os campo necessário para remover um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso exclusão seja feita com sucesso</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult DeletaFilme(int id)
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
