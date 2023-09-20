1ab998cde93ac6f31e2a26aa9927110c3dc3ecad
Mauro <maurojr_@outlook.com>
2023-09-19 21:50:20 -0300

    Adicionando Cinema

---
+++ FilmesAPI/Data/DTOs/UpdateCinemaDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTOs
+{
+    public class UpdateCinemaDto
+    {
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
+        public string Nome { get; set; }
+    }
+}
---
+++ FilmesAPI/Profiles/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.DTOs;
+using FilmesAPI.Models;
+
+namespace FilmesApi.Profiles
+{
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            CreateMap<CreateCinemaDto, Cinema>();
+            CreateMap<Cinema, ReadCinemaDto>();
+            CreateMap<UpdateCinemaDto, Cinema>();
+        }
+    }
+}
