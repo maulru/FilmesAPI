cf41d293f8b3f579857e3c199ac30518876ca01d
Mauro <maurojr_@outlook.com>
2023-09-24 13:50:02 -0300

    Adicionando Endereço

---
+++ FilmesAPI/Data/DTOs/ReadEnderecoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesAPI.Data.DTOs
+{
+    public class ReadEnderecoDto
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public int numero { get; set; }
+    }
+}
---
+++ FilmesAPI/Data/DTOs/UpdateEnderecoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Data.DTOs
+{
+    public class UpdateEnderecoDto
+    {
+        public string Logradouro { get; set; }
+        public int numero { get; set; }
+    }
+}
